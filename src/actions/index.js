b649fe6cd572e94a5ead621ba0eaba3472b15dbe
Michal <michal@Michals-Mac.local>
2019-11-17 08:33:06 +0100

    app works

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,14 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  
+  await dispatch(fetchPosts());
+ const userIds = _.uniq(_.map(getState().posts, 'userId'));
+userIds.forEach(id=>dispatch(fetchUser(id)))
+}
+
 
 export const fetchPosts = () => {
   return async dispatch => {
@@ -10,3 +20,30 @@ export const fetchPosts = () => {
     });
   };
 };
+
+/*
+export const fetchUserMemoized = id => {
+  return dispatch => {
+    _fetchUser(id, dispatch);
+  };
+};
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response
+  });
+});
+*/
+
+export const fetchUser = id => {
+  return async dispatch => { 
+  const response = await jsonPlaceholder(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response
+  });
+}}
